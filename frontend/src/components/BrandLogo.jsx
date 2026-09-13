6709456f0e08b780c20510df1917020afd4de7fd
Kevin <kevinliarte69@gmail.com>
2026-09-13 11:31:06 +0000

    ahora si logo corregido xd

--- frontend/src/components/BrandLogo.jsx
+++ frontend/src/components/BrandLogo.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import acordiaLogo from "../assets/acordia-logo.png";
 
 export default function BrandLogo() {
   return (
     <img
       className="brand-logo"
-      src={`${import.meta.env.BASE_URL}brand/acordia-logo-v1.png`}
+      src={acordiaLogo}
       alt="Acordia"
       width={1983}
       height={793}
